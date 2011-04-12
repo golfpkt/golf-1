cadc8ec574b6b51a6b08248b91651badfbcd6a1d
Julio Capote <jcapote@gmail.com>
2011-04-12 12:57:31 -0700

    fixed bug in test_helper in the way we require simplecov

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,11 @@
 $:.push File.expand_path("../../lib", __FILE__)
 
 require 'rubygems'
-require 'test/unit'
-require 'rack/test'
 require 'simplecov'
 
 SimpleCov.start
 
+
+require 'test/unit'
+require 'rack/test'
 require 'golf'
