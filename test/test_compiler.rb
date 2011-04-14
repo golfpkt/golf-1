d86e17e8757878367e74ad930d32b6ce5d6a64fd
Julio Capote <jcapote@gmail.com>
2011-04-13 19:22:26 -0700

    tests 100% for compiler

--- test/test_compiler.rb
+++ test/test_compiler.rb
@@ -11,18 +11,18 @@ class CompilerTest < Test::Unit::TestCase
     result = OpenStruct.new
     result.components = JSON.parse arr[0].gsub('jQuery.golf.components=','')
     result.res = JSON.parse arr[1].gsub('jQuery.golf.res=','')
+    result.res["components.js"] = "components.js"
     result.plugins = JSON.parse arr[2].gsub('jQuery.golf.plugins=','')
     result.scripts = JSON.parse arr[3].gsub('jQuery.golf.scripts=','')
     result.styles = JSON.parse arr[4].gsub('jQuery.golf.styles=','')
     result
   end
-  
-
 
   def setup
     @compiler = Golf::Compiler.new(File.expand_path("../reference_app", __FILE__))
     @reference_file = File.expand_path("../reference_app/golfapp/components.js", __FILE__)
     @reference = parse_file(@reference_file)
+    
   end
 
 
@@ -37,6 +37,52 @@ class CompilerTest < Test::Unit::TestCase
     b = JSON.parse @compiler.res_json
     assert_equal a,b
   end
+
+
+  def test_res_generation_components
+    a = @reference.res
+    b = JSON.parse @compiler.res_json
+
+    assert_equal a["components"], b["components"]
+    #assert_equal a,b
+  end
+
+  def test_res_generation_plugins
+    a = @reference.res
+    b = JSON.parse @compiler.res_json
+
+    assert_equal a["plugins"], b["plugins"]
+    #assert_equal a["components"], b["components"]
+    #assert_equal a,b
+  end
+
+  def test_res_generation_img
+    a = @reference.res
+    b = JSON.parse @compiler.res_json
+
+    assert_equal a["img"], b["img"]
+    #assert_equal a["components"], b["components"]
+    #assert_equal a,b
+  end
+
+  def test_res_generation_styles
+    a = @reference.res
+    b = JSON.parse @compiler.res_json
+
+    assert_equal a["styles"], b["styles"]
+    #assert_equal a["components"], b["components"]
+    #assert_equal a,b
+  end
+
+  def test_res_generation_scripts
+    a = @reference.res
+    b = JSON.parse @compiler.res_json
+    assert_equal a["scripts"], b["scripts"]
+    #assert_equal a["components"], b["components"]
+    #assert_equal a,b
+  end
+
+
 
   def test_plugin_generation
     a = @reference.plugins
@@ -53,6 +99,7 @@ class CompilerTest < Test::Unit::TestCase
   def test_styles_generation
     a = @reference.styles
     b = JSON.parse @compiler.style_json
+
     assert_equal a,b
   end
 
